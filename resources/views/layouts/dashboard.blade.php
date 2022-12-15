24e3766d4fce59f3c67e3865e8fa92fd117a65ae
CintyaAprilaFandini <cintyaacaf07@gmail.com>
2022-12-15 18:19:32 +0700

    fix layout gcp

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -3,6 +3,7 @@
     data-assets-path="{{ asset('template') }}/assets/" data-template="vertical-menu-template-free">
 
 <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta charset="utf-8" />
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
